c67da4ef53034886d75b76f72fab3bf191156ff4
SHALUSHARMA1997 <111569886+SHALUSHARMA1997@users.noreply.github.com>
2023-05-27 12:30:55 +0530

    Add files via upload

---
+++ movies_on_the_tip/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import './App.css';
+import Header from "./components/Header/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./pages/home/home";
+import MovieList from "./components/movieList/movieList";
+import Movie from "./pages/movieDetail/movie";
+
+
+
+function App() {
+    return(
+        <div className="App">
+            <Router>
+            <Header/>
+                <Routes>
+                    <Route index element = {<Home />}></Route>
+                    <Route path="movie/:id" element={<Movie/>}></Route>
+                    <Route path="movies/:type" element={<MovieList/>}></Route>
+                    <Route path="/*" element={<h1>Error page</h1>}></Route>
+                </Routes>
+            </Router>
+        </div>
+        
+    );
+}
+
+export default App;
